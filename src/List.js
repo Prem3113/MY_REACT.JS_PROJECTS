239585c8d4b9556cc56166293eda12f15d4ad41a
Prem3113 <palavallipremsai@gmail.com>
2022-06-26 15:01:57 +0530

    first commit

---
+++ src/List.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const List = ({people}) => {
+    return(
+        <>
+        {
+            people.map((person) => {
+                const {id,name,age,image} = person;
+                return(
+                    <article className='person' key={id}>
+                        <img src={image} alt={name}></img>
+                        <div>
+                            <h3>{name}</h3>
+                            <p>{age} years</p>
+                        </div>
+
+                    </article>
+                )
+            })
+        }
+        </>
+    )
+}
+export default List;
